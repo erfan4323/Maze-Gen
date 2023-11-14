6a5e7fffd5ffe25df700cb66071497d9bd012393
Erfan_khabir <erfankhakha@gmail.com>
2023-11-14 19:51:07 +0330

    Drawing pixels

--- Gen V1.0/Gen V1.0.cpp
+++ Gen V1.0/Gen V1.0.cpp
@@ -1,20 +1,88 @@
-// Gen V1.0.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+//std includes
 #include <iostream>
+#include <stack>
+
+//renderer
+#define OLC_PGE_APPLICATION
+#include "olcPixelGameEngine.h"
+
+using namespace std;
+
+class Maze : public olc::PixelGameEngine
+{
+public:
+	Maze()
+	{
+		sAppName = "Maze V1.0";
+	}
+
+private:
+	int _mazeWidth;
+	int _mazeHeight;
+	int* _maze;
+
+	enum 
+	{
+		CELL_N = 0x01,
+		CELL_E = 0x02,
+		CELL_S = 0x04,
+		CELL_W = 0x08,
+		CELL_VISITED = 0x10,
+	};
+
+	int _visitedCells;
+
+	stack<pair<int, int>> _stack; // it will stores the path we went in a (x, y) pair
+
+public:
+	// Called once at the start, so create things here
+	bool OnUserCreate() override
+	{
+		//Maze Parameters
+		_mazeHeight = 25;
+		_mazeWidth = 40;
+
+		_maze = new int[_mazeHeight * _mazeWidth];
+		memset(_maze, 0x00, _mazeWidth * _mazeHeight * sizeof(int));
+
+		_stack.push(make_pair(0, 0));
+		_maze[0] = CELL_VISITED;
+		_visitedCells = 1;
+
+		return true;
+	}
+
+	// called once per frame
+	bool OnUserUpdate(float fElapsedTime) override
+	{
+
+		// Rendering ->
+
+		//Clear screen
+		Clear(olc::VERY_DARK_RED);
+
+		//Draw Maze
+		for (int x = 0; x < _mazeWidth; x++)
+		{
+			for (int y = 0; y < _mazeHeight; y++) 
+			{
+				if (_maze[y * _mazeWidth + x] && CELL_VISITED)
+					Draw(x, y, olc::Pixel(olc::WHITE));
+				else
+					Draw(x, y, olc::Pixel(olc::BLUE));
+			}
+		}
+
+		return true;
+	}
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	Maze maze;
+
+	if (maze.Construct(160, 100, 8, 8))
+		maze.Start();
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
